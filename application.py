73628b75e2aaaa4ff456ae14d43c72fd6d56be63
Gustavo Urizar <gustavo.urizar>
2015-03-09 14:17:29 -0600

    added three sentences IF to verify all the numbers inserted.

--- application.py
+++ application.py
@@ -1,4 +1,13 @@
 import random 
 
 GENERATED = random.randint(1, 20)
+print GENERATED
 
+NUMBER = input("insert a number from 1 to 20!!! ")
+
+if NUMBER == GENERATED:
+	print "You Win"
+if NUMBER > GENERATED: 
+	print "The number inserted is higher than generated, please try again."
+if NUMBER < GENERATED: 
+	print "The number inserted is lower than generated, please try again."
