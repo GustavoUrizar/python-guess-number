3ce6a19db4d7652a126b9a4c00a2ec2c5b6657fe
Gustavo Urizar <gustavo.urizar>
2015-03-09 15:36:44 -0600

    funtional code until 4th user story working number of trials

--- application.py
+++ application.py
@@ -3,11 +3,20 @@ import random
 GENERATED = random.randint(1, 20)
 print GENERATED
 
-NUMBER = input("insert a number from 1 to 20!!! ")
 
-if NUMBER == GENERATED:
-	print "You Win"
-if NUMBER > GENERATED: 
-	print "The number inserted is higher than generated, please try again."
-if NUMBER < GENERATED: 
-	print "The number inserted is lower than generated, please try again."
+COUNTER = 1
+while COUNTER <= 4:
+	NUMBER = input("insert a number from 1 to 20!!! ")
+	if NUMBER == GENERATED:
+		print "You Win"
+		AGAIN = raw_input("Do you want to play again, yes or no")
+		if AGAIN == "yes":
+			COUNTER += 1
+		elif AGAIN == "no":
+			break
+	if NUMBER > GENERATED: 
+		print "The number inserted is higher than generated, please try again."
+		COUNTER += 1
+	if NUMBER < GENERATED: 
+		print "The number inserted is lower than generated, please try again."
+		COUNTER += 1
