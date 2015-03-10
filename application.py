e27f8ee3c4a27fb9b932bb4d4c270377116c7405
Gustavo Urizar <gustavo.urizar>
2015-03-10 15:40:07 -0600

    inserted booleanos in while sentences

--- application.py
+++ application.py
@@ -1,22 +1,45 @@
-import random 
+import random
 
-GENERATED = random.randint(1, 20)
-print GENERATED
-
-
-COUNTER = 1
-while COUNTER <= 4:
-	NUMBER = input("insert a number from 1 to 20!!! ")
-	if NUMBER == GENERATED:
-		print "You Win"
-		AGAIN = raw_input("Do you want to play again, yes or no")
-		if AGAIN == "yes":
+NEWGAME = True
+while NEWGAME == True:
+	GENERATED = random.randint(1, 20) #variable that generates the random number. 
+	print GENERATED
+	COUNTER = 1
+	VALIDATE = True
+	while VALIDATE == True:
+		
+		NUMBER = input("insert a number from 1 to 20!!! ")
+		if NUMBER == GENERATED:
+			print "You Win"
+			ANSWER = True
+			while ANSWER == True:
+				AGAIN = raw_input("Do you want to play again, yes or no: ")
+				if AGAIN == "yes":
+					ANSWER = False
+					VALIDATE = False
+					NEWGAME = True
+				elif AGAIN == "no":
+					ANSWER = False
+					VALIDATE = False
+					NEWGAME = False
+		elif NUMBER > GENERATED: 
+			print "The number inserted is higher than generated, please try again."
 			COUNTER += 1
-		elif AGAIN == "no":
-			break
-	if NUMBER > GENERATED: 
-		print "The number inserted is higher than generated, please try again."
-		COUNTER += 1
-	if NUMBER < GENERATED: 
-		print "The number inserted is lower than generated, please try again."
-		COUNTER += 1
+		elif NUMBER < GENERATED: 
+			print "The number inserted is lower than generated, please try again."
+			COUNTER += 1
+		if COUNTER == 5:
+			print "GAME OVER"
+			VALIDATE = False
+			NEWGAME = True
+			ANSWER = True
+			while ANSWER == True:
+				AGAIN = raw_input("Do you want to play again, yes or no: ")
+				if AGAIN == "yes":
+					ANSWER = False
+					VALIDATE = False
+					NEWGAME = True
+				elif AGAIN == "no":
+					ANSWER = False
+					VALIDATE = False
+					NEWGAME = False
