a8daccf3e4df4a1bfb6cc85ebdf07a588458d770
Gustavo Urizar <gustavo.urizar>
2015-03-11 15:32:45 -0600

    inserted try and everything works perfectly

--- application.py
+++ application.py
@@ -1,22 +1,22 @@
 import random
 
 print """WELCOME TO GUESS NUMBER"""
-NEWGAME = True
+NEWGAME = True 
 while NEWGAME == True:
 	GENERATED = random.randint(1, 20) #variable that generates the random number. 
-	print GENERATED
+	print GENERATED 
 	COUNTER = 1
 	VALIDATE = True
 	while VALIDATE == True:
 		COUNTER += 1 #variable that counts the turns
-		if COUNTER <= 5:
-			try: 
-				NUMBER = input("insert a number from 1 to 20!!! ")
-				if NUMBER == GENERATED:
+		if COUNTER <= 5: # if sets the number of turns
+			try: # This try verifies errors in the game 
+				NUMBER = input("insert a number from 1 to 20!!! ") #variable used to ask a number.
+				if NUMBER == GENERATED: #if number is the same than generated the user wins.
 					print "You Win"
 					ANSWER = True
 					while ANSWER == True:
-						AGAIN = raw_input("Do you want to play again, yes or no: ")
+						AGAIN = raw_input("Do you want to play again, yes or no: ") # if the user won this varible is used to ask if the user wants to play again.
 						if AGAIN == "yes" or AGAIN == "y" or AGAIN == "Y" or AGAIN =="YES":
 							ANSWER = False
 							VALIDATE = False
@@ -28,19 +28,19 @@ while NEWGAME == True:
 							NEWGAME = False
 						else:
 							"that is not valid, try again."
-				elif NUMBER > GENERATED: 
+				elif NUMBER > GENERATED: # if the user guesses higher than generated
 					print "The number inserted is higher than generated, please try again."
-				elif NUMBER < GENERATED: 
+				elif NUMBER < GENERATED: # if the user guesses lower than generated
 					print "The number inserted is lower than generated, please try again."
 			except SyntaxError:
 				print "you only can select a number from 1 to 20"
 			except NameError:
 				print "you only can select a number from 1 to 20"
-		else:
+		else: # if the user overcomes the number of trials
 			print "GAME OVER"
 			ANSWER = True
-			while ANSWER == True:
-				AGAIN = raw_input("Do you want to play again, yes or no: ")
+			while ANSWER == True: 
+				AGAIN = raw_input("Do you want to play again, yes or no: ") # after to overcome the number of trials user has the option to play again.
 				if AGAIN == "yes" or AGAIN == "y" or AGAIN == "Y" or AGAIN =="YES":
 					ANSWER = False
 					VALIDATE = False
