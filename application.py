9568ee8e405464e9b9d919b24c9c23fd11b60264
Gustavo Urizar <gustavo.urizar>
2015-03-11 15:12:06 -0600

    inserted try and everything works perfectly

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import random
 
+print """WELCOME TO GUESS NUMBER"""
 NEWGAME = True
 while NEWGAME == True:
 	GENERATED = random.randint(1, 20) #variable that generates the random number. 
@@ -7,39 +8,48 @@ while NEWGAME == True:
 	COUNTER = 1
 	VALIDATE = True
 	while VALIDATE == True:
-		
-		NUMBER = input("insert a number from 1 to 20!!! ")
-		if NUMBER == GENERATED:
-			print "You Win"
-			ANSWER = True
-			while ANSWER == True:
-				AGAIN = raw_input("Do you want to play again, yes or no: ")
-				if AGAIN == "yes":
-					ANSWER = False
-					VALIDATE = False
-					NEWGAME = True
-				elif AGAIN == "no":
-					ANSWER = False
-					VALIDATE = False
-					NEWGAME = False
-		elif NUMBER > GENERATED: 
-			print "The number inserted is higher than generated, please try again."
-			COUNTER += 1
-		elif NUMBER < GENERATED: 
-			print "The number inserted is lower than generated, please try again."
-			COUNTER += 1
-		if COUNTER == 5:
+		COUNTER += 1 #variable that counts the turns
+		if COUNTER <= 5:
+			try: 
+				NUMBER = input("insert a number from 1 to 20!!! ")
+				if NUMBER == GENERATED:
+					print "You Win"
+					ANSWER = True
+					while ANSWER == True:
+						AGAIN = raw_input("Do you want to play again, yes or no: ")
+						if AGAIN == "yes" or AGAIN == "y" or AGAIN == "Y" or AGAIN =="YES":
+							ANSWER = False
+							VALIDATE = False
+							NEWGAME = True
+						elif AGAIN == "no" or AGAIN == "n" or AGAIN == "NO" or AGAIN == "N":
+							print "THANK YOU FOR PLAYING"
+							ANSWER = False
+							VALIDATE = False
+							NEWGAME = False
+						else:
+							"that is not valid, try again."
+				elif NUMBER > GENERATED: 
+					print "The number inserted is higher than generated, please try again."
+				elif NUMBER < GENERATED: 
+					print "The number inserted is lower than generated, please try again."
+			except SyntaxError:
+				print "you only can select a number from 1 to 20"
+			except NameError:
+				print "you only can select a number from 1 to 20"
+		else:
 			print "GAME OVER"
-			VALIDATE = False
-			NEWGAME = True
 			ANSWER = True
 			while ANSWER == True:
 				AGAIN = raw_input("Do you want to play again, yes or no: ")
-				if AGAIN == "yes":
+				if AGAIN == "yes" or AGAIN == "y" or AGAIN == "Y" or AGAIN =="YES":
 					ANSWER = False
 					VALIDATE = False
 					NEWGAME = True
-				elif AGAIN == "no":
+				elif AGAIN == "no" or AGAIN == "n" or AGAIN == "NO" or AGAIN == "N":
+					print "THANK YOU FOR PLAYING"
 					ANSWER = False
 					VALIDATE = False
 					NEWGAME = False
+				else:
+					"that is not valid, try again."		
+	
